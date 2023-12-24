240e5a5fab1a8c6f15f08c649a0400d8db1d404c
KhemchiraP <65160088@go.buu.ac.th>
2023-12-24 15:04:55 +0700

    Homework multiplication table

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
     <?php $multi_x = 12; ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-    <table>
-        <tbody>
-            <?php
-            for( $i=1; $i<=24; $i++ ) {
-                echo "<tr>";
-                echo "<td>" . $multi_x . " x {$i} = " . ($multi_x * $i) . "</td>";
-                echo "</tr>";
-                 
-            }
-        ?>
-        </tbody>
-    </table>
     <style>
-        table{
-          width:150px; 
-          height:80px;  
-          overflow: scroll;
-        }
+    .scroll-box {
+            color: #63320e;
+            width: 200px;
+            height: 400px;
+            overflow: auto;
+            border: 2px solid #C8EAD1;
+            padding: 10px;
+            background-color: #C8EAD1;
+    }
     </style>
+    <body>
+    <div class="scroll-box">
+        <?php
+        for ($i=1; $i<=24; $i++) {
+            echo "<p>";
+            echo "<td>" . $multi_x . " x {$i} = " . ($multi_x * $i) . "</td>";
+            echo "</p>";
+        }
+        ?>
+    </div>
 </body>
 
 </html>
